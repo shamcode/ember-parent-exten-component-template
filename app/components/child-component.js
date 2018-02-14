e9b08ce366793cbd7c4ed6d03eb12cfe457ecf89
Eugeny Burnashov <shamcode@gmail.com>
2018-02-14 12:51:55 +0700

    Partials

--- app/components/child-component.js
+++ app/components/child-component.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import ParentComponent from './parent-component';
 
 export default ParentComponent.extend({
-  parentComponent: 'parent-component',
-  age: Ember.computed.oneWay('user.age')
+  parentComponent: 'components/parent-component',
+  age: Ember.computed.oneWay('user.age'),
+  agePartial: 'components/child-component/partials/age',
 })
--- app/components/parent-component.js
+++ app/components/parent-component.js
@@ -2,5 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   firstName: Ember.computed.oneWay('user.firstName'),
-  lastName: Ember.computed.oneWay('user.lastName')
+  lastName: Ember.computed.oneWay('user.lastName'),
+  firstNamePartial: 'components/parent-component/partials/first-name',
+  lastNamePartial: 'components/parent-component/partials/last-name',
+  agePartial: 'noop'
 });
